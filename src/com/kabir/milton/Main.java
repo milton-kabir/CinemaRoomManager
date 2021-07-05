5a873c05a0ab9616d37100a309a543f9d624b94b
Milton Kabir <drifter.kabir@gmail.com>
2021-07-05 23:19:20 +0600

    Third Stage completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -11,14 +11,61 @@ public class Main {
         int x=sc.nextInt();
         System.out.println("Enter the number of seats in each row:");
         int y= sc.nextInt();
-        System.out.println("Total income:");
+        String[] ar=new String[x+1];
+        ar[0]=" ";
+        for(int i=1;i<=y;i++){
+            ar[0]+=+i;
+        }
+        for(int i=1;i<=x;i++){
+            ar[i]="";
+            for(int j=0;j<=y;j++){
+                if(j==0){
+                    ar[i]+=i;
+                }
+                else{
+                    ar[i]+="S";
+                }
+            }
+        }
+        System.out.println("Cinema:");
+        for(int i=0;i<=x;i++){
+            for(int j=0;j<=y;j++){
+                System.out.print(ar[i].charAt(j)+" ");
+            }
+            System.out.println();
+        }
+        int xx,yy;
+        System.out.println("Enter a row number:");
+        xx=sc.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        yy=sc.nextInt();
         if(x*y<=60){
-            System.out.println("$"+x*y*10);
+            System.out.println("Ticket price: $10");
+            ar[xx]=ar[xx].substring(0,yy)+'B'+ar[xx].substring(yy+1);
+            System.out.println("Cinema:");
+            for(int i=0;i<=x;i++){
+                for(int j=0;j<=y;j++){
+                    System.out.print(ar[i].charAt(j)+" ");
+                }
+                System.out.println();
+            }
         }
         else{
             int x1=x/2;
-            int x2=x-x1;
-            System.out.println("$"+(x1*10+x2*8)*y);
+            if(xx<=x1){
+                System.out.println("Ticket price: $10");
+            }
+            else{
+                System.out.println("Ticket price: $8");
+            }
+            ar[xx]=ar[xx].substring(0,yy)+'B'+ar[xx].substring(yy+1);
+            System.out.println("Cinema:");
+            for(int i=0;i<=x;i++){
+                for(int j=0;j<=y;j++){
+                    System.out.print(ar[i].charAt(j)+" ");
+                }
+                System.out.println();
+            }
         }
     }
 }
